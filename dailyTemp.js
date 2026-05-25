20ca1eb718a2fa083f8a233c3460ac4d0b1f4efa
saurav903 <sk8517167@gmail.com>
2026-05-26 00:31:10 +0530

    daily temperature dsa

---
+++ dailyTemp.js
@@ -0,0 +1,35 @@
+var dailyTemperatures = function(temperatures) {
+    let n = temperatures.length;
+    let stack = [];
+     stack.push(n-1);
+    let arr = new Array(n).fill(0);
+    for(let i=n-2; i>=0; i--){
+        while(stack.length){
+            if(temperatures[i]>=temperatures[stack[stack.length-1]]){
+                stack.pop()
+            }else{
+                arr[i]=stack[stack.length-1]-i;
+                break;
+            }
+        }
+        stack.push(i)
+    }
+    return arr;
+};
+
+// Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+// Example 1:
+
+// Input: temperatures = [73,74,75,71,69,72,76,73]
+// Output: [1,1,4,2,1,1,0,0]
+// Example 2:
+
+// Input: temperatures = [30,40,50,60]
+// Output: [1,1,1,0]
+// Example 3:
+
+// Input: temperatures = [30,60,90]
+// Output: [1,1,0]
