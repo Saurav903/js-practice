98e4e8f9c232aeaea809e4ae9858849955ecd654
saurav903 <sk8517167@gmail.com>
2026-05-28 00:50:08 +0530

    reduce polyfills changed

--- reduce.js
+++ reduce.js
@@ -18,3 +18,31 @@ console.log(val);
 // let val = arr.reduce((acc,item)=>{
 //   acc + item
 // },0)
+
+// Updated reducer for some edge cases like this
+// [1,2,3,4].myReduce((a,b) => a*b, 1) // works
+// [1,2,3,4].myReduce((a,b) => a*b, 0) // broken logic
+
+// argument => it like how many arguments has been passed to function call
+
+let startIndex;
+    let acc;
+    if ((arguments.length < 2 && !this.length)) {
+        throw new TypeError("called on null or undefined");
+    }
+    if ((typeof callback !== "function")) {
+        throw new TypeError("callback not a function");
+    }
+    if (arguments.length >= 2) {
+        acc = initialValue;
+        startIndex = 0;
+    } else {
+        acc = this[0];
+        startIndex = 1;
+    }
+    for (let i = startIndex; i <= this.length-1; i++) {
+        if (i in this) {
+        acc = callback(acc, this[i],i,this);
+        }
+    }
+    return acc;
